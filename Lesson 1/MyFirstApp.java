bf4759dddb3c25e3723480b87c75455e80695d44
MaksimS02 <sankov.m@yandex.ru>
2021-01-19 10:30:15 +0100

    new map

---
+++ Lesson 1/MyFirstApp.java
@@ -0,0 +1,7 @@
+public class MyFirstApp {
+    public static void main (String[] args) {
+        System.out.println("Hello, world");
+        System.out.println("я программист");
+        System.out.println("I am a robot");
+    }
+}
---
+++ Lesson 1/Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+    public static void main (String[] args) {
+        short win = 10;
+        int b = 64;
+        char a = 'C';
+        byte cores = 12;
+        double freguency = 3.7;
+        float ram = 8.00f;
+        boolean systemstarted = true;
+        long directx = 12;
+        System.out.println("Windows " + win +" " + b + " bit");
+        System.out.println("Windows Disk "+ a);
+        System.out.println("Processor A10-7850K cores " + cores);
+        System.out.println("Processor freguency " + freguency);
+        System.out.println("Ram GB " + ram);
+        System.out.println("DirectX Version "+ directx);
+        System.out.println("System Started " + systemstarted);
+
+    }
+}
