fe66f9fa600b376450d6ad759eebd277fbeac404
MaksimS02 <sankov.m@yandex.ru>
2021-01-19 12:27:51 +0100

    clear mistakes2

--- Lesson 1/Variables.java
+++ Lesson 1/Variables.java
@@ -1,20 +1,20 @@
 public class Variables {
-    public static void main (String[] args) {
-        short win = 10;
-        int b = 64;
-        char a = 'C';
-        byte cores = 12;
-        double freguency = 3.7;
-        float ram = 8.00f;
-        boolean systemstarted = true;
-        long directx = 12;
-        System.out.println("Windows " + win +" " + b + " bit");
-        System.out.println("Windows Disk "+ a);
-        System.out.println("Processor A10-7850K cores " + cores);
+    public static void main(String[] args) {
+        short windows=10;
+        int bitrate=64;
+        char windowsdisk='C';
+        byte processorcores=12;
+        double freguency=3.7;
+        float ram=8.00f;
+        boolean systemstarted=true;
+        long directx=12;
+
+        System.out.println("Windows " + windows + " " + bitrate + " bit");
+        System.out.println("Windows Disk " + windowsdisk);
+        System.out.println("Processor A10-7850K cores " + processorcores);
         System.out.println("Processor freguency " + freguency);
         System.out.println("Ram GB " + ram);
-        System.out.println("DirectX Version "+ directx);
+        System.out.println("DirectX Version " + directx);
         System.out.println("System Started " + systemstarted);
-
     }
 }
