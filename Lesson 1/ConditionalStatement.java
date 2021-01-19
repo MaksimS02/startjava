34cad5c951e19b2c52c2f16437e69e8da74c8cd3
MaksimS02 <sankov.m@yandex.ru>
2021-01-19 16:33:08 +0100

    clear mistakes3

--- Lesson 1/ConditionalStatement.java
+++ Lesson 1/ConditionalStatement.java
@@ -1,35 +1,32 @@
 public class ConditionalStatement {
     public static void main(String[] args) {
-        int age=25;
+        int age = 25;
         if (age > 20) { 
-        System.out.println ("adult");
+            System.out.println ("adult");
         }
 
-        boolean male=true;
+        boolean male = true;
         if (male) { 
-        System.out.println ("men"); 
+            System.out.println ("men");
         }
         if (!male) { 
-        System.out.println ("women"); 
+            System.out.println ("women");
         }
 
-        double high=1.86;
+        double high = 1.86;
         if (high < 1.80) { 
-        System.out.println ("small"); 
-        }        
-        else {
-        System.out.println ("tall");
+            System.out.println ("small");
+        } else {
+            System.out.println ("tall");
         }
 
-        char firstletter='I';
+        char firstletter = 'I';
         if (firstletter == 'M') {
-        System.out.println ("First Letter is M");
-        }    
-        if (firstletter == 'I') {
-        System.out.println ("First Letter is I");
-        }
-        else {
-        System.out.println ("not letter M or I");
+            System.out.println ("First Letter is M");
+        } else if (firstletter == 'I') {
+            System.out.println ("First Letter is I");
+        } else {
+            System.out.println ("not letter M or I");
         }
     }
 }
