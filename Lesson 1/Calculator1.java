39e516b7e885b9e46578b194bc88f2cfd73aa9fc
MaksimS02 <sankov.m@yandex.ru>
2021-01-20 18:16:30 +0100

    newcalc

---
+++ Lesson 1/Calculator1.java
@@ -0,0 +1,35 @@
+public class Calculator {
+    public static void main(String args[]){
+        int num1 = 15;
+        int num2 = 9;
+        long result = 0;
+        int i = 1;
+        char operation = '^';
+
+        if (operation == '+'){
+            result = num1 + num2;
+                System.out.println("Your result =" + result);
+        }
+        else if (operation == '-'){
+            result = num1 - num2;
+                System.out.println("Your result =" + result);
+        }
+        else if (operation == '*'){
+            result = num1 * num2;
+                System.out.println("Your result =" + result);
+        }
+        else if (operation == '/'){
+            result = num1 / num2;
+                System.out.println("Your result =" + result);
+        }
+        else if (operation == '^'){
+            result = num1;
+            for(i = 1; i < num2; i++){
+               result = result * num1;
+            }
+            
+        }
+                System.out.println("Your result =" + result);
+    }
+}
+
