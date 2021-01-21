761a2cf107cbfcbb72e32fcc37b0bd375bd8c0b4
MaksimS02 <sankov.m@yandex.ru>
2021-01-21 20:33:35 +0100

    change2103

--- Lesson 1/Cycle.java
+++ Lesson 1/Cycle.java
@@ -1,26 +1,26 @@
 public class Cycle {
     public static void main(String[] args) {
-            System.out.println ("TASK 1");
+        System.out.println ("TASK 1");
         int i;
         for(i = 0; i < 21 ; i++ )
             System.out.println (i);
 
-            System.out.println ("TASK 2");
+        System.out.println ("TASK 2");
         int e = 6;
-        while (e >= -6){
+        while (e >= -6) {
             System.out.println (e);
-        e=e-2;
+        e-=2;
         }
-        
-            System.out.println ("TASK 3");
+
+        System.out.println ("TASK 3");
         int b = 10;
         int sum = 0;
         do {
             b++;
-            if(b % 2 != 0){
+            if(b % 2 != 0) {
                 sum +=b;
-        }
+            }
         } while (b < 20);
-            System.out.println("Summ Odd Number " + sum);
+        System.out.println("Summ Odd Number " + sum);
     }
 }
