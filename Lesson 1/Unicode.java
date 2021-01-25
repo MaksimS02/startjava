cdd6df1da19953c19ce1fc2d5a45f5f9bbd611ff
MaksimS02 <sankov.m@yandex.ru>
2021-01-25 09:10:32 +0100

    chage2402

--- Lesson 1/Unicode.java
+++ Lesson 1/Unicode.java
@@ -1,6 +1,6 @@
 public class Unicode {
     public static void main(String[] args) {
-        for (int c = 33; c <=126; c++)
-            System.out.println((char)c);
+        for (int c = 33; c <= 126; c++)
+            System.out.println((char) c);
     }
 }
