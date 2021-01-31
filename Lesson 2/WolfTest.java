cfd52f9ae8c564d4a2f3d990cd32899b4b590a83
MaksimS02 <sankov.m@yandex.ru>
2021-01-31 13:21:51 +0100

    change31011

--- Lesson 2/WolfTest.java
+++ Lesson 2/WolfTest.java
@@ -1,21 +1,16 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf wolfOne = new Wolf();
-        System.out.println("nickName = " + wolfOne.nickName);
-        System.out.println("sex = " + wolfOne.sex);
-        System.out.println("color = " + wolfOne.color);
-        System.out.println("weight = " + wolfOne.weight);
-        System.out.println("age = " + wolfOne.age);
-        wolfOne.nickName = "Silver";
-        wolfOne.sex = "alien";
-        wolfOne.color = "red";
-        wolfOne.weight = - 500;
-        wolfOne.age = 1530;
-        System.out.println("nickName = " + wolfOne.nickName);
-        System.out.println("sex = " + wolfOne.sex);
-        System.out.println("color = " + wolfOne.color);
-        System.out.println("weight = " + wolfOne.weight);
-        System.out.println("age = " + wolfOne.age);
+        wolfOne.setNickName("Cold");
+        System.out.println("nickName = " + wolfOne.getNickName());
+        wolfOne.setColor("female");
+        System.out.println("sex = " + wolfOne.getSex());
+        wolfOne.setColor("red");
+        System.out.println("color = " + wolfOne.getColor());
+        wolfOne.setWeight(85.6f);
+        System.out.println("weight = " + wolfOne.getWeight());
+        wolfOne.setAge(10);
+        System.out.println("age = " + wolfOne.getAge());
         wolfOne.go();
         wolfOne.sit();
         wolfOne.run();
